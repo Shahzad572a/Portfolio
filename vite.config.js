7ee7dc43b71185e1c94ea650261cbe75cc848fa5
Shahzad Ali <shahzad572a@gmail.com>
2023-10-24 15:33:39 +0500

    .

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,31 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/your-app-base/', // Set the base path for your app
+  publicDir: 'public', // Specify the directory where your public assets are located
+
+  // Define additional build options
+  build: {
+    outDir: 'dist', // Set the output directory for the built files
+    assetsDir: 'assets', // Specify the directory for static assets within the output directory
+    rollupOptions: {
+      // Customize Rollup options if needed
+    },
+  },
+
+  server: {
+    port: 3000, // Set the development server port
+  },
 })
